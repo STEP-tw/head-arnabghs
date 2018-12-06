435262b4c2b86ce3f346f3b3493a473321c5c251
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2018-12-06 11:36:15 +0530

    implemented loop in head function in headLib

--- src/headLib.js
+++ src/headLib.js
@@ -1,10 +1,15 @@
 const head = function(argv,fs){
-  let fileName = argv[2];
-  if(!fs.existsSync(fileName)) 
-    return 'head: '+fileName+': No such file or directory';
-  let content = fs.readFileSync(fileName,'utf-8');
+  let fileNames = argv.slice(2);
+
+  let trimmedLines = [];
+  for (file of fileNames){
+  if(!fs.existsSync(file)) 
+    return 'head: '+file+': No such file or directory';
+  let content = fs.readFileSync(file,'utf-8');
   let lines = content.split('\n');
-  return lines.slice(0,10).join('\n');
+    trimmedLines = lines.slice(0,10);
+  }
+  return trimmedLines.join('\n');
 }
 
 module.exports = { head };
