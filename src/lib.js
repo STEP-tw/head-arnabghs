1257773990a51330e3aeafe0b5523ace32f59417
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2018-12-17 15:31:01 +0530

    removed a unnecessary line from lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,81 +1,66 @@
-const {
-  readUserInput
-} = require('./parse.js');
+const { readUserInput } = require("./parse.js");
 
-const {
-  checkErrors
-} = require('./error.js');
+const { checkErrors } = require("./error.js");
 
-const formatContent = function (command, fs, fileNames, type, count) {
-  let getContent = function (path) {
+const formatContent = function(command, fs, fileNames, type, count) {
+  let getContent = function(path) {
     if (!fs.existsSync(path))
-      return command + ': ' + path + ': No such file or directory';
-    let content = fs.readFileSync(path, 'utf-8');
-    let getChar = (command == 'tail') ? getLastNChars : getFirstNChars;
-    let getLine = (command == 'tail') ? getLastNLines : getFirstNLines;
-    let get = (type == 'byte') ? getChar : getLine;
+      return command + ": " + path + ": No such file or directory";
+    let content = fs.readFileSync(path, "utf-8");
+    let getChar = command == "tail" ? getLastNChars : getFirstNChars;
+    let getLine = command == "tail" ? getLastNLines : getFirstNLines;
+    let get = type == "byte" ? getChar : getLine;
     return get(content, count);
-  }
-  let getContentWithTitle = function (path) {
+  };
+  let getContentWithTitle = function(path) {
     if (!fs.existsSync(path)) return getContent(path);
-    return ["==> " + path + " <==", getContent(path)].join('\n');
-  }
+    return ["==> " + path + " <==", getContent(path)].join("\n");
+  };
   if (fileNames.length == 1) return getContent(fileNames[0]);
-  return fileNames.map(getContentWithTitle).join('\n');
-}
+  return fileNames.map(getContentWithTitle).join("\n");
+};
 
-const formatContentForHead = formatContent.bind(null, 'head')
-const formatContentForTail = formatContent.bind(null, 'tail')
+const formatContentForHead = formatContent.bind(null, "head");
+const formatContentForTail = formatContent.bind(null, "tail");
 
-const head = function (argv, fs) {
-  let {
-    fileNames,
-    count,
-    type
-  } = readUserInput(argv);
-  let {
-    errorExist,
-    errorMsg
-  } = checkErrors('head', count, type);
+const head = function(argv, fs) {
+  let { fileNames, count, type } = readUserInput(argv);
+  let { errorExist, errorMsg } = checkErrors("head", count, type);
   if (errorExist) return errorMsg;
-  count = +count;
   return formatContentForHead(fs, fileNames, type, count);
-}
+};
 
+const getFirstNLines = function(content, numberOfLines) {
+  return content
+    .split("\n")
+    .slice(0, numberOfLines)
+    .join("\n");
+};
 
-const getFirstNLines = function (content, numberOfLines) {
-  return content.split('\n').slice(0, numberOfLines).join('\n');
-}
+const getFirstNChars = function(content, numberOfChars) {
+  return content
+    .split("")
+    .slice(0, numberOfChars)
+    .join("");
+};
 
-const getFirstNChars = function (content, numberOfChars) {
-  return content.split('').slice(0, numberOfChars).join('');
-}
-
-const tail = function (argv, fs) {
-  let {
-    fileNames,
-    count,
-    type
-  } = readUserInput(argv);
-  let {
-    errorExist,
-    errorMsg
-  } = checkErrors('tail', count, type);
+const tail = function(argv, fs) {
+  let { fileNames, count, type } = readUserInput(argv);
+  let { errorExist, errorMsg } = checkErrors("tail", count, type);
   if (errorExist) return errorMsg;
   count = Math.abs(+count);
   return formatContentForTail(fs, fileNames, type, count);
-}
+};
 
-const getLastNLines = function (content, numberOfLines) {
-  let lines = content.trim().split('\n');
-  return lines.slice(-numberOfLines).join('\n');
-}
+const getLastNLines = function(content, numberOfLines) {
+  let lines = content.trim().split("\n");
+  return lines.slice(-numberOfLines).join("\n");
+};
 
-const getLastNChars = function (content, numberOfChars) {
-  let chars = content.split('');
-  return chars.slice(-numberOfChars).join('');
-}
-
+const getLastNChars = function(content, numberOfChars) {
+  let chars = content.split("");
+  return chars.slice(-numberOfChars).join("");
+};
 
 module.exports = {
   head,
