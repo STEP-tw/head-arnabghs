fb4bcebff35da16416ed9845ffd052a7cb7ca049
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2018-12-18 10:29:36 +0530

    extracted getCountAndFileNames from readUserInput function

--- src/parse.js
+++ src/parse.js
@@ -1,18 +1,22 @@
+const getCountAndFilenames = function(argv, userInput) {
+  userInput.count = argv[2].slice(2);
+  userInput.fileNames = argv.slice(3);
+  if (argv[2].length == 2) {
+    userInput.count = argv[3];
+    userInput.fileNames = argv.slice(4);
+  }
+  return userInput;
+};
+
 const readUserInput = function(argv) {
-  const userInput = {
-    fileNames: argv.slice(2),
-    count: 10,
-    option: "line"
-  };
+  let userInput = { fileNames: argv.slice(2), count: 10, option: "line" };
   if (argv[2].startsWith("-c")) {
-    userInput.count = argv[2].length == 2 ? argv[3] : argv[2].slice(2);
-    userInput.fileNames = argv[2].length == 2 ? argv.slice(4) : argv.slice(3);
+    userInput = getCountAndFilenames(argv, userInput);
     userInput.option = "byte";
     return userInput;
   }
   if (argv[2].startsWith("-")) {
-    userInput.count = argv[2].length == 2 ? argv[3] : argv[2].slice(2);
-    userInput.fileNames = argv[2].length == 2 ? argv.slice(4) : argv.slice(3);
+    userInput = getCountAndFilenames(argv, userInput);
     if (!isNaN(argv[2].charAt(1))) {
       userInput.count = argv[2].slice(1);
       userInput.fileNames = argv.slice(3);
